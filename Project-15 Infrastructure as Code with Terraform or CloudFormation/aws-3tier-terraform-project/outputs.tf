0d69b1aab835e60d4b37034935227d6df6260b0c
Prasanna Velan V <prasannavelan2003@gmail.com>
2026-05-15 22:32:41 +0530

    Add files via upload

---
+++ Project-15 Infrastructure as Code with Terraform or CloudFormation/aws-3tier-terraform-project/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.project_bucket.bucket
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql_db.endpoint
+}
